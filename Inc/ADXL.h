bf8612010c5ccfc1347b232c30fb900e38d7d7d4
Lucky <Lucky@212.25.137.42>
2018-12-04 14:42:24 +0100

    done

---
+++ Inc/ADXL.h
@@ -0,0 +1,15 @@
+/*
+ * ADXL.h
+ *
+ *  Created on: 20 Nov 2018
+ *      Author: Lucky
+ */
+
+#ifndef ADXL_H_
+#define ADXL_H_
+
+void ADXL_Init();
+void ADXL_WriteRegister();
+void ADXL_ReadRegister();
+void ADXL_ReadRegisters();
+#endif /* ADXL_H_ */
---
+++ Src/ADXL.c
@@ -0,0 +1,16 @@
+/*
+ * ADXL.c
+ *
+ *  Created on: 20 Nov 2018
+ *      Author: Lucky
+ */
+
+#include "ADXL.h"
+#include "stdio.h"
+#define ADXL_adress 0x53<<1
+
+uint8_t uniqueID = 0;
+
+void ADXL_Init() {
+
+}
